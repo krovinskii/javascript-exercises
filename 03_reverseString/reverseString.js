533c3b1c2995755175f253ace3bc55bd5affb015
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-02 15:40:01 -0600

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(preSplit) {
+let postSplit = preSplit.split('');
+let reversedSplit = postSplit.reverse();
+let complete = reversedSplit.join('');
+return complete;
 
-};
-
+}
+console.log(reverseString("christmas"))
 // Do not edit below this line
 module.exports = reverseString;
