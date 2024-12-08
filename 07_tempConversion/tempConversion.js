0244341b99ed6c82a6703adad6005d25c52bb658
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-08 12:48:47 -0600

    Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  cTemp = (fTemp - 32) * 5/9
+
+  if (Number.isInteger(cTemp)) {
+    return cTemp
+  }
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cToFTemp) {
+  const fTempConverted = (cToFTemp * 9/5) + 32;
+  if (Number.isInteger(fTempConverted)) {
+    return fTempConverted
+  }
+  return parseFloat(fTempConverted.toFixed(1));
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(73.2))
+
+// npm test tempConversion.spec.js
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
