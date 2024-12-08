71aaed4c9692a30258fd25c33c671b4a4a2c5860
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-08 12:04:22 -0600

    finished problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+        });
+        // and return that array
+        
+        return newArray;
+        
+      };
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(minArray, maxArray) {
+    if (!Number.isInteger(minArray) || !Number.isInteger(maxArray)) {
+        return 'ERROR';
+      }
+    if (minArray <= 0 || maxArray <= 0) {
+        return 'ERROR';
+    }
 
-};
+let numArray = [];
 
+if (minArray > maxArray) {
+    [minArray, maxArray] = [maxArray, minArray];
+}
+
+for (let i = minArray; i <= maxArray; i++) {
+    numArray.push(i);
+}
+numArray.reverse();
+return numArray.reduce((partialSum, a) => partialSum + a, 0)
+}
+
+
+console.log(sumAll(123, 1))
 // Do not edit below this line
 module.exports = sumAll;
