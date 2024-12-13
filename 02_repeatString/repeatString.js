f246407ed508d823c9050867435e560ec2eb191b
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-13 13:28:16 -0600

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, x) {
+    let result = '';
+    if (x < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= x; i++) {
+        result += str
+    }
+    
+    return result;
 };
 
+console.log(repeatString("butt", -1));
 // Do not edit below this line
 module.exports = repeatString;
