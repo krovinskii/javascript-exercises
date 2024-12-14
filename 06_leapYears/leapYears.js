494d9e08493e22dd84d11141c6be53ed1a181fb8
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-14 13:21:06 -0600

    Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisibleByFour = year / 4;
+    divisibleByHundred = year / 100;
+    divisibleByFourHundred = year / 400;
 
+    if (Number.isInteger(divisibleByFour) && !Number.isInteger(divisibleByHundred) ) {
+        return true
+    };
+    if (Number.isInteger(divisibleByFourHundred)) {
+        return true
+    };
+    return false;
 };
-
+console.log(leapYears(1900))
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(startWithFahrenheit) {
+let FToC = (startWithFahrenheit - 32) * 5/9;
+let completeC = Math.round(FToC * 10) / 10;
+return completeC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(startWithCelsius) {
+  let CToF = (startWithCelsius * 9/5) + 32;
+  let completeF = Math.round(CToF * 10) / 10;
+  return completeF
 };
 
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
