dcbc8d41ec575a0c052a80db039d08918f4e814d
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-14 14:07:35 -0600

    Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    for (let i = 0; i < cleanedStr.length / 2; i++) {
+        if (cleanedStr[i] !== cleanedStr[cleanedStr.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
-
+console.log(palindromes('tacos'));
 // Do not edit below this line
 module.exports = palindromes;
+
+
+//Need to iterate through beginning and end, beginning === end at all times,
+//if not then it is not a palindrome. need to ignore punctuation, spaces,etc
