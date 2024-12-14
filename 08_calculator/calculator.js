53af90014e4606d11c2ded34db3227902deb18d4
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-14 13:54:51 -0600

    Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum1 = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum1 += arr[i];
+  }
+  return sum1;
+
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product * num, 1)
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if  (num === 0 || num === 1) {
+    return 1;
+  }
+	for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
