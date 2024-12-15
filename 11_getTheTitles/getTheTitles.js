c359e03969668178296f91d78ddc8cd2d72cfb02
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-15 09:35:44 -0600

    complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let desiredProperty = (books, desiredKey) => books.map((element) => element[desiredKey]);
+    let desiredKey = "title";
+    return desiredProperty(books, desiredKey); 
 };
 
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+];
+
+let result = getTheTitles(books);
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
