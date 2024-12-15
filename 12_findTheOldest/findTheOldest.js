62d9faef05fc1ca3c6bd8457392d86352b553bd0
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-15 09:59:02 -0600

    complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let oldestPerson = people[0]; 
+    let maxAge = 0;  
+  
+    for (let i = 0; i < people.length; i++) {
+      const person = people[i];
+      let age;
+  
+      if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+      } else {
+        age = currentYear - person.yearOfBirth;
+      }
+      if (age > maxAge) {
+        maxAge = age;
+        oldestPerson = person;  
+      }
+    }
+  
+    return oldestPerson; 
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
